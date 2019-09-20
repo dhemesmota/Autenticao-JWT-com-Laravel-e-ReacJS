f5f7c3d6ef39937b4ff9c6f52380b2a0e996a28f
Dhemes Mota <dhemes.mota@gmail.com>
2019-09-20 13:40:41 -0300

    Update user list

--- frontend/src/pages/Home/index.js
+++ frontend/src/pages/Home/index.js
@@ -1,37 +1,74 @@
 import React, { useEffect, useState, Fragment } from 'react';
 import api from '../../services/api';
 import { logout } from '../../services/auth';
+import { parseISO, format, formatRelative, formatDistance } from 'date-fns';
+import {pt} from 'date-fns/locale';
 
 import NavBar from '../components/NavBar';
 
 export default function Home({ history}) {
 
-    const [ name, setName ] = useState('');
+    const [ users, setUsers ] = useState([]);
+    const [user, setUser] = useState([]);
 
-    async function handleUser() {
-        try {
-            const response = await api.get('/user');
-            const { user } = response.data;
-            setName(user.name);
-            //console.log(user.name);
-        } catch(err) {
-            //console.log(err.response.status);
-            logout();
-        }
-    }
+    
 
     useEffect(() => {
         document.title = "Home | My App";
 
-        handleUser();
+        async function handleUsers() {
+
+            const response = await api.get('/users').catch(err => {
+                alert('Sessão inspirou! Faça login novamente.');
+                logout();
+            });
+            //const dados = response.data.json();
+            if (response) {
+                console.log(response.data);
+                setUsers(response.data);
+            }
+            
+            await api.get('/user').then(dados => {
+                console.log(dados.data);
+                setUser(dados.data.user);
+            }).catch(error => {
+                alert(`Error: ${error}`);
+            });
+        }
+
+
+        handleUsers();
     },[]);
 
     return(
         <Fragment>
-            <NavBar history={history} />
-            <div className="container-fluid">
-                {name && <p className="text-right my-3 text-secondary">{name}</p>}
-                <h5>Usuários</h5>
+            <NavBar history={history} username={user.name} />
+            <div className="container">
+                <h5 className="my-3">Bem vindo(a), {user.name}!</h5>
+
+                <table className="table">
+                    <thead>
+                        <tr>
+                        <th scope="col">#</th>
+                        <th scope="col">Nome</th>
+                        <th scope="col">Email</th>
+                        <th scope="col">Cadastrado</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {users.map(user => (
+                            <tr key={user.id}>
+                                <th scope="row">{user.id}</th>
+                                <td>{user.name}</td>
+                                <td>{user.email}</td>
+                                <td>{formatRelative(parseISO(user.created_at), new Date(), {locale: pt})}</td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+
+                <h1>
+                </h1>
             </div>
         </Fragment>
     );
